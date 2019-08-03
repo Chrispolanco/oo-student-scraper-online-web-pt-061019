7b4d24b0e1b77fb639c84255a8a89bfc123b1296
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-03 21:15:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,8 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attr, value|
       self.send("#{attr}", value)
-    end 
+    end
+    self 
   end 
 
   def self.all
